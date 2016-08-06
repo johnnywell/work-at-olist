2bbb66da466950472cc49e68b3781bacba9e91c6
Johnny Wellington dos Santos <johnny.w.santos@gmail.com>
2016-08-06 20:28:26 -0300

    Prepare dev environment

---
+++ dev-provision.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# The ubuntu/xenial64 is missing the virtualbox-guest-utils, so we need to
+# install it in order to get synced folders to work.
+sudo apt-get update
+sudo apt-get install -y virtualbox-guest-utils
+
+# Install compatible version of setuptools dev
+sudo apt-get install -y python3-setuptools python3-dev
+sudo easy_install3 pip
+
+# Install PostgreSQL
+sudo apt-get install -y postgresql libpq-dev build-essential
+sudo su postgres -c "createuser olist -s"
