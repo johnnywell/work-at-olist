81f9f627b140bb1ba94ff7fa1cf84ec8bc85fe59
Johnny Wellington dos Santos <johnny.w.santos@gmail.com>
2016-08-13 20:06:18 -0300

    Add channel and category slug formation tests

--- work-at-olist/channels/tests.py
+++ work-at-olist/channels/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from channels.models import Channel, Category
+from django.utils.text import slugify
 
-# Create your tests here.
+class ChannelTestCase(TestCase):
+
+    def test_slug_creation_rule(self):
+        """
+        The slug is very important on this application,
+        so we must garantee its formation rule is correct.
+        """
+        name = "Amazon"
+        channel = Channel.objects.create(name=name)
+        self.assertEqual(channel.slug, self.construct_slug(channel))
+
+    def construct_slug(self, channel):
+        return slugify(channel.name)
+
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        channel = Channel.objects.create(name="Amazon")
+
+    def test_slug_creation_rule(self):
+        name = 'Science Fiction'
+        channel = Channel.objects.get(name="Amazon")
+        parent = Category.objects.create(name="Books", channel=channel)
+        self.assertEqual(parent.slug, self.construct_slug(parent))
+        category = Category.objects.create(name=name, channel=channel, parent=parent)
+        self.assertEqual(category.slug, self.construct_slug(category))
+
+    def construct_slug(self, category):
+        if category.parent:
+            slug = "{}-{}-{}".format(category.channel, category.parent.name, category.name)
+        else:
+            slug = "{}-{}".format(category.channel, category.name)
+        return slugify(slug)
