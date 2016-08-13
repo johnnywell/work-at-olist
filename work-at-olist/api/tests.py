41aa11bd40b0cb474310d57f3b789828dbe56d6d
Johnny Wellington dos Santos <johnny.w.santos@gmail.com>
2016-08-13 20:02:50 -0300

    Add channel API creation test

--- work-at-olist/api/tests.py
+++ work-at-olist/api/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User, Group
 
-# Create your tests here.
+
+class APIChannelTest(APITestCase):
+    fixtures = ['contenttypes.json', 'auth.json', 'channels.json']
+
+    def test_create_channel(self):
+        """
+        Ensure we can create a new channel object.
+        """
+        url = reverse('channel-list')
+        data = {"name": "AliExpress"}
+        user = User.objects.get(username='tester')
+        self.client.force_authenticate(user=user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {'categories': 'http://testserver/api/channels/aliexpress/categories/', 'url': 'http://testserver/api/channels/aliexpress/', 'name': 'AliExpress'})
